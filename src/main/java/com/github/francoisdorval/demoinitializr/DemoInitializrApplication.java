1d9947a657d76530ed650455f813aca871406f77
François Dorval <francois.dorval@gmail.com>
2021-12-13 10:19:08 +0100

    first commit

---
+++ src/main/java/com/github/francoisdorval/demoinitializr/DemoInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.github.francoisdorval.demoinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/francoisdorval/demoinitializr/DemoInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.francoisdorval.demoinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
